62fa02c078a38b7c7f90aaae1053dc387ce53191
RohitParmar-17 <rohitghost5050@gmail.com>
2025-02-12 21:56:20 +0530

    dev4

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,20 +1,42 @@
+// import React from "react";
+// import Typewriter from "typewriter-effect";
+
+// function Type() {
+//   return (
+//     <Typewriter
+//       options={{
+//         strings: [
+//           "Software Developer",
+//           "3rd Year Student",
+//           "MERN Stack Developer",
+//         ],
+//         autoStart: true,
+//         loop: true,
+//         deleteSpeed: 50,
+//       }}
+//     />
+//   );
+// }
+
+// export default Type;
+
 import React from "react";
-import Typewriter from "typewriter-effect";
+import { TypeAnimation } from "react-type-animation";
 
 function Type() {
   return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software Developer",
-          "3rd Year Student",
-          "MERN Stack Developer",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
+    <div className="TypeAnimation">
+      <TypeAnimation
+        sequence={[
+          "Software Developer", 1000, 
+          "3rd Year Student", 1000, 
+          "MERN Stack Developer", 1000
+        ]}
+        speed={50}
+        wrapper="span"
+        repeat={Infinity}
+      />
+    </div>
   );
 }
 
