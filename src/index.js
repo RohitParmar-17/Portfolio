cef8349690db5d93397eddf7d1badbec6d00dda6
RohitParmar-17 <rohitghost5050@gmail.com>
2025-02-12 21:23:29 +0530

    again before deploy

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
-
-// reportWebVitals();
-
 import React from "react";
-import ReactDOM from "react-dom/client"; // Use 'react-dom/client' instead of 'react-dom'
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// Create root using React 18's createRoot method
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
+
 reportWebVitals();
+
+// import React from "react";
+// import ReactDOM from "react-dom/client"; // Use 'react-dom/client' instead of 'react-dom'
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+
+// // Create root using React 18's createRoot method
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
